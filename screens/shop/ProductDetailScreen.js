b2d645807ce1195de55e1073b7aeba4bacc96d82
Avani Patel <anpatel36@yahoo.in>
2021-06-04 14:47:07 +0530

    Working on the Product Detail Screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { ScrollView, Text, View, Image, Button, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Colors from '../../constants/Colors';
+
 const ProductDetailScreen = props => {
 
     const productId = props.navigation.getParam('productId');
@@ -10,9 +12,14 @@ const ProductDetailScreen = props => {
     );
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+             <View style={styles.actions}>
+                 <Button color={Colors.primary} title="Add To Cart" onPress={() => { }} />
+             </View>
+             <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+             <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 };
 
@@ -23,6 +30,25 @@ ProductDetailScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    }
 });
 
 export default ProductDetailScreen;
