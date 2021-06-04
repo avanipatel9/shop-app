0cb4d0051a660d1cb972c3ad5429c4a54209f0fd
Avani Patel <anpatel36@yahoo.in>
2021-06-04 17:56:05 +0530

    Adding Logic to Delete Items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, FlatList, StyleSheet, Button, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cartAction';
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -18,8 +19,12 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a, b) =>
+            a.productId > b.productId ? 1 : -1
+        );
     });
+
+    const dispatch = useDispatch();
 
     return (
         <View style={styles.screen}>
@@ -36,7 +41,16 @@ const CartScreen = props => {
             <FlatList
                 data={cartItems}
                 keyExtractor={item => item.productId}
-                renderItem={itemData => <CartItem quantity={itemData.item.quantity} title={itemData.item.productTitle} amount={itemData.item.sum} onRemove={() => {}} /> }
+                renderItem={itemData => (
+                    <CartItem
+                        quantity={itemData.item.quantity}
+                        title={itemData.item.productTitle}
+                        amount={itemData.item.sum}
+                        onRemove={() => {
+                            dispatch(cartActions.removeFromCart(itemData.item.productId));
+                        }}
+                    />
+                )}
             />
         </View>
     )
--- store/reducers/cartReducer.js
+++ store/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import {ADD_TO_CART} from '../actions/cartAction';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cartAction';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
@@ -7,15 +7,15 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_TO_CART: 
+    switch (action.type) {
+        case ADD_TO_CART:
             const addedProduct = action.product;
             const productPrice = addedProduct.price;
             const productTitle = addedProduct.title;
 
             let updatedOrNewCartItem;
 
-            if(state.items[addedProduct.id]) {
+            if (state.items[addedProduct.id]) {
                 //already have the item in the cart
                 updatedOrNewCartItem = new CartItem(
                     state.items[addedProduct.id].quantity + 1,
@@ -30,7 +30,29 @@ export default (state = initialState, action) => {
                 ...state,
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + productPrice
+            };
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.productId];
+            const currentQty = selectedCartItem.quantity;
+            let updatedCartItems;
+            if (currentQty > 1) {
+                //need to reduce it, not erase it
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1,
+                    selectedCartItem.productPrice,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                );
+                updatedCartItems = {...state.items, [action.productId]: updatedCartItem};
+            } else {
+                updatedCartItems = { ...state.items };
+                delete updatedCartItems[action.productId];
             }
+            return {
+                ...state,
+                items: updatedCartItems,
+                totalAmount: state.totalAmount - selectedCartItem.productPrice
+            };
     }
     return state;
 }
