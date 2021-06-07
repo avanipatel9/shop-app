49675fe7bd06c76f5756bfd0584ea2da0babe12f
Avani Patel <anpatel36@yahoo.in>
2021-06-07 20:18:30 +0530

    Making the Show Details Button Work

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 import CartItem from './CartItem';
 import Colors from '../../constants/Colors';
 
 const OrderItem = props => {
-    return(
+    const [showDetails, setShowDetails] = useState(false);
+
+    return (
         <View style={styles.orderItem}>
             <View style={styles.summary}>
                 <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
                 <Text style={styles.date}>{props.date}</Text>
             </View>
-            <Button color={Colors.primary} title="Show Details" />
+            <Button color={Colors.primary} title={showDetails ? 'Hide Details' : 'Show Details'} onPress={() => {
+                setShowDetails(previousState => !previousState)
+            }}
+            />
+            {showDetails && <View style={styles.detailItems}>
+                {props.items.map(cartItem => (
+                    <CartItem
+                        key={cartItem.productId}
+                        quantity={cartItem.quantity}
+                        amount={cartItem.sum}
+                        title={cartItem.productTitle}
+                    />))}
+            </View>}
         </View>
     )
 }
 
-const styles=StyleSheet.create({
-    orderItem : {
+const styles = StyleSheet.create({
+    orderItem: {
         shadowColor: 'black',
         shadowOpacity: 0.6,
         shadowOffset: { width: 0, height: 2 },
@@ -29,21 +43,24 @@ const styles=StyleSheet.create({
         padding: 10,
         alignItems: 'center'
     },
-    summary : {
+    summary: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         width: '100%',
         marginBottom: 15
     },
-    totalAmount : {
+    totalAmount: {
         fontFamily: 'open-sans-bold',
         fontSize: 16
     },
-    date : {
+    date: {
         fontSize: 16,
         fontFamily: 'open-sans',
         color: '#888'
+    },
+    detailItems: {
+        width: '100%'
     }
 });
 
