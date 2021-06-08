87a08e1fe6a2b2d51724dcc6580de910b1e9fb12
Avani Patel <anpatel36@yahoo.in>
2021-06-09 01:34:29 +0530

    Reorganizing the ProductItem Component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
-
-import Colors from '../../constants/Colors';
+import { View, Text, Image, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 
 const ProductItem = props => {
 
@@ -14,7 +12,7 @@ const ProductItem = props => {
     return (
         <View style={styles.product}>
             <View style={styles.touchable}>
-                <TouchableComponent onPress={props.onViewDetail} useForeground >
+                <TouchableComponent onPress={props.onSelect} useForeground >
                     <View>
                         <View style={styles.imageContainer}>
                             <Image style={styles.image} source={{ uri: props.image }} />
@@ -24,8 +22,7 @@ const ProductItem = props => {
                             <Text style={styles.price}>${props.price.toFixed(2)}</Text>
                         </View>
                         <View style={styles.actions}>
-                            <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
-                            <Button color={Colors.primary} title="Add To Cart" onPress={props.onAddToCart} />
+                            {props.children}
                         </View>
                     </View>
                 </TouchableComponent>
