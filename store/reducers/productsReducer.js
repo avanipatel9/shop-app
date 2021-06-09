7bcda415d38373a31c85cb05d6cd25808443480b
Avani Patel <anpatel36@yahoo.in>
2021-06-09 22:41:00 +0530

    Deleting Items

--- store/reducers/productsReducer.js
+++ store/reducers/productsReducer.js
@@ -1,10 +1,23 @@
 import PRODUCTS from '../../data/dummy-data';
+import { DELETE_PRODUCT } from '../actions/productsAction';
 
- const initialState = {
-     availableProducts: PRODUCTS,
-     userProducts: PRODUCTS.filter(product => product.ownerId === 'u1')
- }
+const initialState = {
+    availableProducts: PRODUCTS,
+    userProducts: PRODUCTS.filter(product => product.ownerId === 'u1')
+}
 
- export default (state = initialState, action) => {
-     return state;
- } 
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case DELETE_PRODUCT:
+            return {
+                ...state,
+                userProducts: state.userProducts.filter(
+                    product => product.id !== action.productId
+                ),
+                availableProducts: state.availableProducts.filter(
+                    product => product.id !== action.productId
+                )
+            };
+    }
+    return state;
+}
