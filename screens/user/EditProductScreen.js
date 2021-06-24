20b365fecc52bf3f780d2f002fd4dc6d644fca14
Avani Patel <anpatel36@yahoo.in>
2021-06-24 14:48:55 +0530

    Handling Additional Errors

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useCallback, useReducer } from 'react';
-import { View, ScrollView, StyleSheet, Platform, Alert, KeyboardAvoidingView } from 'react-native';
+import React, { useState, useEffect, useCallback, useReducer } from 'react';
+import { View, ScrollView, StyleSheet, Platform, Alert, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import * as productsActions from '../../store/actions/productsAction';
 import Input from '../../components/UI/Input';
+import Colors from '../../constants/Colors';
 
 const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
 
@@ -33,6 +34,8 @@ const formReducer = (state, action) => {
 };
 
 const EditProductScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const prodId = props.navigation.getParam('productId');
     const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === prodId));
@@ -55,33 +58,46 @@ const EditProductScreen = props => {
         formIsValid: editedProduct ? true : false,
     });
 
-    const submitHandler = useCallback(() => {
+    useEffect(() => {
+        if(error) {
+            Alert.alert('An error occurred!', error, [{text: 'Okay'}]);
+        }
+    }, [error])
+
+    const submitHandler = useCallback(async () => {
         if (!formState.formIsValid) {
             Alert.alert('Wrong input', 'Please check the errors in the form.', [
                 { text: 'Okay' }
             ])
             return;
         }
-        if (editedProduct) {
-            dispatch(
-                productsActions.updateProduct(
-                    prodId,
-                    formState.inputValues.title,
-                    formState.inputValues.description,
-                    formState.inputValues.imageUrl
-                )
-            );
-        } else {
-            dispatch(
-                productsActions.createProduct(
-                    formState.inputValues.title,
-                    formState.inputValues.description,
-                    formState.inputValues.imageUrl,
-                    +formState.inputValues.price
-                )
-            );
+        setError(null);
+        setIsLoading(true);
+        try {
+            if (editedProduct) {
+                await dispatch(
+                    productsActions.updateProduct(
+                        prodId,
+                        formState.inputValues.title,
+                        formState.inputValues.description,
+                        formState.inputValues.imageUrl
+                    )
+                );
+            } else {
+                await dispatch(
+                    productsActions.createProduct(
+                        formState.inputValues.title,
+                        formState.inputValues.description,
+                        formState.inputValues.imageUrl,
+                        +formState.inputValues.price
+                    )
+                );
+            }
+            props.navigation.goBack();
+        } catch(err) {
+            setError(err.message);
         }
-        props.navigation.goBack();
+        setIsLoading(false);
     }, [dispatch, prodId, formState]);
 
     useEffect(() => {
@@ -96,6 +112,14 @@ const EditProductScreen = props => {
             input: inputIdentifier
         });
     }, [dispatchFormState]);
+
+    if(isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        )
+    }
 
     return (
         <KeyboardAvoidingView
@@ -182,6 +206,11 @@ EditProductScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
     form: {
         margin: 20
+    },
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
 
